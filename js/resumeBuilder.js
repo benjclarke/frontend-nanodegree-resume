4ff080f83ea4f4e68de6a71d4fd2b3fd040f8560
Benjamin Clarke <ben@theshipyard.com>
2015-11-10 08:00:50 -0500

    added test text

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,7 @@
-$('#main').append('Benjamin Clarke');
+$('#main').append("Benjamin Clarke");
 
-var formattedName = HTMLheaderName.replace('%data%', Benjamin Clarke);
-var formattedRole = HTMLheaderRole.replace('%data%', Person);
+var formattedName = HTMLheaderName.replace("%data%", Benjamin Clarke);
+var formattedRole = HTMLheaderRole.replace("%data%", Person);
 
 $('#header').append(formattedName);
 $('#header').append(formattedRole);
