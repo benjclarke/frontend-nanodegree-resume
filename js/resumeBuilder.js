3e1bda7f865680638a8482d7c83adede69f8d674
Benjamin Clarke <ben@theshipyard.com>
2015-11-13 06:36:52 -0500

    added bio

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,5 +3,20 @@ var formattedName = HTMLheaderName.replace("%data%", name);
 var role = "Developer";
 var formattedRole = HTMLheaderRole.replace("%data%", role);
 
+var bio = {
+	"name": "Benjamin Clarke", 
+	"role": "Original Gangster", 
+	"contact_info": {
+		"phone": "614-607-1010", 
+		"address": "580 North Fourth Street", 
+		"email": "name@email.com", 
+		}, 
+		"welcomeMessage" : "Welcome to the Internet", 
+	"skills": ["leadership", "sales", "python", "data science" ], 
+	"picture": "/pictures/ben.jpg" 
+
+}
+
 $("#header").append(formattedName);
 $("#header").append(formattedRole);
+$("#main").append(bio);
