edabf6ebef99ab4f455dfceb67c9a0616139017f
Benjamin Clarke <ben@theshipyard.com>
2015-11-13 07:17:05 -0500

    changed to topcontacts

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -26,7 +26,6 @@ $("#header").append(formattedRole);
 var formattedMobile = HTMLmobile.replace("%data%", bio.contact.mobile); 
 var formattedWelcomeMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
  
-console.log(bio.welcomeMessage);
-$("header").append(formattedMobile); 
-$("main").append(formattedWelcomeMessage); 
+$("topContacts").append(formattedMobile); 
+$("topContacts").append(formattedWelcomeMessage); 
 
