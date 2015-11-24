60ad729007f201641b69136d4db68d55c397e5bf
Benjamin Clarke <ben@theshipyard.com>
2015-11-24 13:39:51 -0500

    no message

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,10 +1,8 @@
-var name = "Benjamin Clarke";
-var formattedName = HTMLheaderName.replace("%data%", name);
-var role = "Developer";
-var formattedRole = HTMLheaderRole.replace("%data%", role);
+//var name = "Benjamin Clarke";
+//var formattedName = HTMLheaderName.replace("%data%", name);
+//var role = "Developer";
+//var formattedRole = HTMLheaderRole.replace("%data%", role);
 
-$("#header").append(formattedName);
-$("#header").append(formattedRole);
 
 
 $("#main").append(internationalizeButton);
@@ -22,52 +20,26 @@ var bio = {
 		}, 
 		"welcomeMessage" : "Welcome to the Test Page", 
 	"skills": ["leadership", "sales", "python", "data science" ], 
-	"picture": "http://placehold.it/400X250" 
+	"picture": "http://placehold.it/400X250", 
+	display: function() {
+		var formattedName = HTMLheaderName.replace("%data%", bio.name);
+		var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+		$("#header").prepend(formattedName);
+		$("#header").prepend(formattedRole);
+	};
 
 };
 
-var formattedMobile = HTMLmobile.replace("%data%", bio.contact.mobile); 
-var formattedAddress = HTMLcontactGeneric.replace("%data%", bio.contact.address);
-var formattedEmail = HTMLemail.replace("%data%", bio.contact.email);
-var formattedGitHub = HTMLgithub.replace("%data%", bio.contact.github);
-var formattedTwitter = HTMLtwitter.replace("%data%", bio.contact.twitter);
-var formattedLocation = HTMLlocation.replace("%data%", bio.contact.location);
-var formattedWelcomeMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
-var formattedImage = HTMLbioPic.replace("%data%", bio.picture);
-
-$("#header").append(formattedMobile);
-$("#header").append(formattedAddress);
-$("#header").append(formattedEmail);
-$("#header").append(formattedGitHub);
-$("#header").append(formattedTwitter);
-$("#header").append(formattedLocation); 
-$("#header").append(formattedWelcomeMessage); 
-$("#header").append(formattedImage);
-
-
-
-var work = {
-
-	jobs: [
-	{
-"position": "Regular Jo",
-"employer": "Genormous",
-"dates": "2014-2015",
-"location": "Ohio",
-"description": "worked as a guy building out suppliers"
-		}
-	]
-};
 
 var education = {
     "schools": [
         {
             "name": "Ohio State",
-            "city": "Columbus",
+            "location": "Columbus",
             "degree": "BS",
             "major": "Genetics",
-            "minor": "finance",
-            "graduated": "2009"
+            "dates": "2009",
+            "url": "www.osu.edu"
         }
     ],
     "onlineCourses": [
@@ -77,20 +49,45 @@ var education = {
             "dates": 2015,
             "url": "udacity.com"
         }
-    ]
+    ],
+    //display: function() {
+
+    };
+}
+
+var work = {
+
+	jobs: [
+	{
+		"position": "Regular Jo",
+		"employer": "Genormous",
+		"dates": "2014-2015",
+		"location": "Ohio",
+		"description": "worked as a guy building out suppliers"
+		}
+	],
+	//display: function() {
+
+	}
+
 };
 
-var projects = {
-	"project": [
-		{
-			"title": "My Test Project",
-			"start": 2015,
-			"description": "a resume project for myself",
-			"dates": "April 2016",
-			"url": "http://placehold.it/500X350"
-		}
-	]
-};
+
+
+var project = {
+	projects: [
+	{
+		"title": "My Test Project",
+		"start": 2015,
+		"description": "a resume project for myself",
+		"dates": "April 2016",
+		"url": "http://placehold.it/500X350"
+	}
+	], 
+	//display: function() {
+
+	}
+}
 
 if (bio.skills.length > 0) {
 	$("#header").append(HTMLskillsStart);
@@ -120,19 +117,29 @@ for (var i = 0; i < work.jobs.length; i++) {
 
 	}
 
-	projects.display = function( ) {
+	project.display = function( ) {
 
-		for(i=0; i < projects.project.length; i++) {
+		for(i=0; i < project.length; i++) {
 		$("#projects").append("HTMLprojectStart");
-		var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.project[i].title);
-		$("project-entry:last").append(formattedProjectTitle);
-		var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.project[i].dates);
-		$("project-entry:last").append(formattedProjectDates);
-		var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.project[i].description);
-		$("project-entry:last").append(formattedProjectDescription);
-		var formattedProjectURL = HTMLprojectImage.replace("%data%", projects.project[i].url);
-		$("project-entry:last").append(formattedProjectURL);
 	}
-	};
+	}
 
-$("#mapDiv").append(map)
+var formattedMobile = HTMLmobile.replace("%data%", bio.contact.mobile); 
+var formattedAddress = HTMLcontactGeneric.replace("%data%", bio.contact.address);
+var formattedEmail = HTMLemail.replace("%data%", bio.contact.email);
+var formattedGitHub = HTMLgithub.replace("%data%", bio.contact.github);
+var formattedTwitter = HTMLtwitter.replace("%data%", bio.contact.twitter);
+var formattedLocation = HTMLlocation.replace("%data%", bio.contact.location);
+var formattedWelcomeMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+var formattedImage = HTMLbioPic.replace("%data%", bio.picture);
+
+$("#header").append(formattedMobile);
+$("#header").append(formattedAddress);
+$("#header").append(formattedEmail);
+$("#header").append(formattedGitHub);
+$("#header").append(formattedTwitter);
+$("#header").append(formattedLocation); 
+$("#header").append(formattedWelcomeMessage); 
+$("#header").append(formattedImage);
+
+bio.display();
